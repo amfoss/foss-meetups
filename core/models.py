5abf513ebcdf6ee4c414a5db1df1d3e69d34c257
Chirath R <chirath.02@gmail.com>
2018-04-02 22:53:38 +0530

    Models

--- core/models.py
+++ core/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 
 class Team(models.Model):
-    admin = models.OneToOneField(User, on_delete=models.SET_NULL)
+    admin = models.OneToOneField(
+        User, on_delete=models.SET_NULL, null=True, related_name='admin')
     team_name = models.CharField(max_length=100)
     tag_line = models.CharField(max_length=300)
     description = models.TextField()
@@ -18,10 +19,24 @@ class Team(models.Model):
     irc = models.CharField(max_length=100, blank=True)
 
     created_date = models.DateTimeField(auto_created=True)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='created_by')
 
     def __str__(self):
         return self.team_name
+
+
+class Speaker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=50)
+    organisation = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+    github = models.URLField()
+
+    about = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
 
 
 class Meetup(models.Model):
@@ -35,24 +50,14 @@ class Meetup(models.Model):
     is_open = models.BooleanField(default=False)
     is_open_till = models.DateTimeField()
 
-    team = models.ForeignKey(User, on_delete=models.SET_NULL)
+    team = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='team')
 
     created_date = models.DateTimeField(auto_created=True)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
-
-
-class Speaker(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=50)
-    organisation = models.CharField(max_length=200)
-    designation = models.CharField(max_length=200)
-    github = models.URLField()
-
-    def __str__(self):
-        return self.user.get_full_name()
 
 
 class Talk(models.Model):
@@ -68,3 +73,31 @@ class Talk(models.Model):
     def __str__(self):
         return self.name
 
+
+class MeetupPhoto(models.Model):
+    meetup = models.ForeignKey(Meetup, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.meetup.name
+
+
+class MeetupComments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meetup = models.ForeignKey(Meetup, on_delete=models.CASCADE)
+    # parent = models.IntegerField(default=0)
+    comment = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.meetup.name
+
+
+class CallForPaper(models.Model):
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    meetup = models.ForeignKey(Meetup, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=300)
+    abstract = models.TextField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.topic
