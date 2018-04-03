5a47acd6137e02f52e929f3e4e243aa67c2de74d
Chirath R <chirath.02@gmail.com>
2018-04-03 18:34:23 +0530

    Add media and static urls

--- meetup/urls.py
+++ meetup/urls.py
@@ -25,8 +25,5 @@ urlpatterns = [
     url(r'', include('core.urls')),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
